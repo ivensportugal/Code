a0904cfadcb0c8936716f646b33398469e93b70d
Ivens Portugal <ivensportugal@me.com>
2018-08-18 18:02:25 -0400

    update pointers

--- pointers2.cpp
+++ pointers2.cpp
@@ -6,16 +6,41 @@ void some_function(int* pi, int i) {
 	cout << "pi: " << *pi << endl;
 }
 
+void some_other_function(int* pi){
+
+	int i = 111;
+	cout << "fakepi: " << &pi << endl;
+	pi = &i;
+	// *pi = i;
+}
+
 int main() {
 
 	int i = 10;
-	int* pi;
-	cout << "create &pi: " << &pi << endl;
-	cout << "create  pi: " <<  pi << endl;
-	cout << "create *pi: " << *pi << endl;
-	*pi = i;
+	int* pi = &i;
+	// int* pj = &i;
 
-	some_function(pi, i);
+	cout << "BEFORE" << endl;
+
+	cout << "&pi: " << &pi << endl;
+	cout << " pi: " <<  pi << endl;
+	cout << "*pi: " << *pi << endl;
+
+	// cout << "&pj: " << &pj << endl;
+	// cout << " pj: " <<  pj << endl;
+	// cout << "*pj: " << *pj << endl;
+
+	some_other_function(pi);
+
+	cout << "AFTER" << endl;
+
+	cout << "&pi: " << &pi << endl;
+	cout << " pi: " <<  pi << endl;
+	cout << "*pi: " << *pi << endl;
+
+	// cout << "&pj: " << &pj << endl;
+	// cout << " pj: " <<  pj << endl;
+	// cout << "*pj: " << *pj << endl;
 
 	return 0;
 	
